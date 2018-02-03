eaf7355b1bf7031e314d90063e5a7d5452ff422f
wata_mac <watassbass@gmail.com>
2018-02-03 23:57:15 +0900

    Enable HEROGATE_DEBUG

--- app.go
+++ app.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"os"
+
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"github.com/wata727/herogate/command"
 )
 
 // NewApp return new application
 func NewApp() *cli.App {
+	if os.Getenv("HEROGATE_DEBUG") == "1" || os.Getenv("HEROGATE_DEBUG") == "true" {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
 	app := cli.NewApp()
 	app.Name = Name
 	app.Usage = "Deploy and manage containerized applications like Heroku on AWS"
